acc4980e1dfed0a17ecc7ea5caa2f417e236e2b4
Kageorgis <40515478+Kageorgis@users.noreply.github.com>
2021-11-16 00:06:06 -0800

    Changed LWC

--- force-app/main/default/lwc/warnerforceLandingPageComp/warnerforceLandingPageComp.js
+++ force-app/main/default/lwc/warnerforceLandingPageComp/warnerforceLandingPageComp.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
+import COE from '@salesforce/resourceUrl/COEDiagram';
 
-export default class WarnerforceLandingPageComp extends LightningElement {}
+export default class WarnerforceLandingPageComp extends LightningElement {
+
+    COEDiagram = COE;
+
+}
